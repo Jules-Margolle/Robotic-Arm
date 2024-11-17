1d587076c21733905ea416bcf2e208a79e97d9b0
Jules-Margolle <ju.margo62630@gmail.com>
2024-11-17 17:49:14 +0100

    init maven project

---
+++ ArmSoftWare/src/main/java/project/RoboticArm/App.java
@@ -0,0 +1,22 @@
+package project.RoboticArm;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        primaryStage.show();
+    }
+    public static void main( String[] args )
+    {
+       launch(args);
+    }
+}
